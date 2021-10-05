558cec78781424d49e3658a171fe96bf93c48d94
VolodymyrVorona <volodimir.vorona@gmail.com>
2021-10-05 12:39:07 +0300

    fixed button component

--- src/components/MainButton/MainButton.jsx
+++ src/components/MainButton/MainButton.jsx
@@ -1,27 +1,17 @@
-import st from './MainButton.module.scss';
+import '../../styles/components/_MainButton.scss';
 
-const MainButton = ({ type, clickHandler = null, text, name }) => {
-  let currentClassName = '';
-  // eslint-disable-next-line default-case
-  switch (name) {
-    case 'register':
-      currentClassName = st.register;
-      break;
-
-    case 'main':
-      currentClassName = st.main;
-      break;
-
-    case 'balance':
-      currentClassName = st.balance;
-      break;
-  }
-
-  return (
-    <button className={currentClassName} type={type} onClick={clickHandler}>
-      {text}
-    </button>
-  );
-};
+const MainButton = ({ type, clickHandler = null, text, className, accent }) => (
+  <button
+    className={accent ? className + ' accent' : className}
+    type={type}
+    onClick={clickHandler}
+  >
+    {text}
+  </button>
+);
 
 export default MainButton;
+
+// =================== EXAMPLE ===============================
+
+//  <MainButton text="button test" className="register" | "main" | "balance" clickHandler={sum handler} accent />

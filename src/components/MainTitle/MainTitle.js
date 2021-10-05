c8dc1f8517dced9c8db6079eabd578af8054a2c8
Nataliia-Blazhko <blazhko.natasha91@gmail.com>
2021-10-05 23:24:39 +0300

    добавляет миксины на медиа квери и компонент мейн тайтл

---
+++ src/components/MainTitle/MainTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainTitle = () => {
+  return (
+    <div>
+      <h1>Kapu$ta</h1>
+      <p>Smart finance</p>
+    </div>
+  );
+};
+
+export default MainTitle;

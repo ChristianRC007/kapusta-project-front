6fadfe8fe37e2997c4372f3fe89792b3d399db31
Nataliia-Blazhko <blazhko.natasha91@gmail.com>
2021-10-05 17:09:40 +0300

    добавляет исправления работы с бекграундом в десктопной версии

--- src/components/Backgrounds/BackgroundLogin.js
+++ src/components/Backgrounds/BackgroundLogin.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import backgroundLarge from '../../assets/images/kapustas.png';
-import backgroundSmall from '../../assets/images/kapusta.png';
 
 const BackgroundLogin = () => {
   return (
     <>
-      <div
-        style={{ backgroundImage: `url(${backgroundLarge})` }}
-        className="background background__login-top"
-      ></div>
-      <div
-        style={{ backgroundImage: `url(${backgroundSmall})` }}
-        className="background background__login-bottom"
-      ></div>
+      <div className="background background__login-top"></div>
+      <div className="background background__login-bottom"></div>
     </>
   );
 };
--- src/components/Backgrounds/BackgroundMain.js
+++ src/components/Backgrounds/BackgroundMain.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import backgroundLarge from '../../assets/images/kapustas.png';
 
 const BackgroundMain = () => {
   return (
     <>
       <div className="background background__main-top"></div>
-      <div
-        style={{ backgroundImage: `url(${backgroundLarge})` }}
-        className="background background__main-bottom"
-      ></div>
+      <div className="background background__main-bottom"></div>
     </>
   );
 };

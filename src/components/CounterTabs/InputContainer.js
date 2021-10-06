19dbeb20ce40fa4a40bba421bda29865687d4d7f
AntonLovkin <anton28111987@gmail.com>
2021-10-06 20:36:00 +0300

    инпуты в один компонент

--- src/components/CounterTabs/InputContainer.js
+++ src/components/CounterTabs/InputContainer.js
@@ -1,13 +1,33 @@
-import InputCounter from './InputCounter';
-import SelectCounter from './SelectCounter';
-import PayInput from './PayInput';
+import React, { useState } from 'react';  
+import Select from 'react-select'
+
+const options = [
+  { value: 'products', label: 'Products' },
+  { value: 'salary', label: 'Salary' },
+  { value: 'drinks', label: 'Drinks' }
+]
 
 export default function InputContainer() {
+  const [productName, setProductName] = useState('');
+  const [payValue, setPayValue] = useState('');
+  
   return (
-        <div className='input-container'>
-      <InputCounter />
-      <SelectCounter />
-      <PayInput />
-        </div>       
+    <div className='input-container'>
+      <div >  
+      <form className='form-input'>  
+        <input className='form-input' placeholder='Описание товара' type="text" 
+            onChange={(e) => setProductName(e.target.value)} required />
+          
+        <Select className='form-input' placeholder='Категория товара' options={options} />
+          <input className='form-input' placeholder='00,0' type="text" onChange={(e) => setPayValue(e.target.value)} />
+
+        <svg class="icon-input" width="20" height="20">
+          <use  href="../../images/calculator.svg">
+          </use>
+          </svg>
+          
+      </form>  
+    </div> 
+  </div>       
   );
 }

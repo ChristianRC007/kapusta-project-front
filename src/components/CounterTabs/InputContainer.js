6d0d0ea7019ea7131a7a7d04014e43177707ef1b
AntonLovkin <anton28111987@gmail.com>
2021-10-06 22:56:41 +0300

    ((

--- src/components/CounterTabs/InputContainer.js
+++ src/components/CounterTabs/InputContainer.js
@@ -15,16 +15,17 @@ export default function InputContainer() {
     <div className='input-container'>
       <div >  
       <form className='form-input'>  
-        <input className='form-input' placeholder='Описание товара' type="text" 
+        <input className='input-productName' placeholder='Описание товара' type="text" 
             onChange={(e) => setProductName(e.target.value)} required />
           
-        <Select className='form-input' placeholder='Категория товара' options={options} />
-          <input className='form-input' placeholder='00,0' type="text" onChange={(e) => setPayValue(e.target.value)} />
+          <Select className='select-category' placeholder='Категория товара' options={options} />
+          
+        <input className='input-productValue' placeholder='00,0' type="text" onChange={(e) => setPayValue(e.target.value)} />
 
-        <svg class="icon-input" width="20" height="20">
+        {/* <svg class="icon-input" width="20" height="20">
           <use  href="../../images/calculator.svg">
           </use>
-          </svg>
+          </svg> */}
           
       </form>  
     </div> 

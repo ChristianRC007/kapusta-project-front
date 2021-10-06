8584209bcaff571cc80efcc7f4ef6a7c63b4d5b5
VolodymyrVorona <volodimir.vorona@gmail.com>
2021-10-06 18:34:16 +0300

    Changed button class

--- src/components/GoogleAuth/GoogleAuthButton.jsx
+++ src/components/GoogleAuth/GoogleAuthButton.jsx
@@ -1,7 +1,7 @@
-import { ReactComponent as GoogleIcon } from './google-symbol.svg';
+import { ReactComponent as GoogleIcon } from '../../assets/img/google.svg';
 
 const GoogleAuthButton = ({ onClick, disabled }) => (
-  <button className="btn" onClick={onClick} disabled={disabled}>
+  <button className="button_google" onClick={onClick} disabled={disabled}>
     <GoogleIcon className="googleSvg" />
     Google
   </button>

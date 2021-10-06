00a12675f401aa68c7b6cc4d26164fd3b1352627
Max Gavryshkiv <maks20355@gmail.com>
2021-10-07 00:19:07 +0300

    Created bttn goBack

--- src/components/MainHeadPannel/MainHeadPannel.jsx
+++ src/components/MainHeadPannel/MainHeadPannel.jsx
@@ -1,3 +1,22 @@
+import React from 'react';
+import { useHistory, NavLink } from 'react-router-dom';
+
 export default function MainHeadPannel() {
-  return <div>Balance pannel</div>;
+  const history = useHistory();
+
+  function handleClick() {
+    history.push(history.goBack());
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={handleClick}>
+        Вернуться на главную
+      </button>
+      Balance pannel
+      <NavLink exact to="/report">
+        Перейти к отчетам
+      </NavLink>
+    </div>
+  );
 }

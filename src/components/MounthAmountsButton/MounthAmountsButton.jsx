fbb4fc76a0beaa3702cb12cfb2b73bd1d133dd75
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-10-08 20:49:03 +0200

    add html file and scss

---
+++ src/components/MounthAmountsButton/MounthAmountsButton.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export default function MounthAmountsButton() {
+    
+
+    return (
+             <div className='currentmb'>
+            <div className='currentmb__container'>
+                 <p className='currentmb__title'>Текущий период:</p>
+                <div className="currentmb__buttons">
+                <button className='currentmb__btn'>
+                    <svg width="7" height="12" viewBox="4 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 1L6 6L10 11" stroke="#FF751D" stroke-width="2"/>
+                    </svg>
+                </button>
+                    <p className='currentmb__mounth'>Ноябрь 2019</p>
+                <button className='currentmb__btn'>
+                    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 1L5 6L1 11" stroke="#FF751D" stroke-width="2"/>
+                    </svg>
+                </button>
+                </div>
+               </div>
+            </div>
+    )
+}
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -2,11 +2,14 @@
 import MainHeadPannel from '../../components/MainHeadPannel';
 import BackgroundMain from '../../components/Backgrounds/BackgroundMain'
 import Header from '../../components/Header/Header';
+import MounthAmountsButton from '../../components/MounthAmountsButton';
+
 export default function MainLayout({ children }) {
   return (
     <>
       <Header />
-      <BackgroundMain/>
+      <BackgroundMain />
+      <MounthAmountsButton/>
       <MainHeadPannel />
       <div className="container">{children}</div>
     </>

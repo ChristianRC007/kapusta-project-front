bb571d98559734b4c626dbb7e393281638426d1c
Nataliia-Blazhko <blazhko.natasha91@gmail.com>
2021-10-10 23:26:49 +0300

    добавляет разметку ReportHeader (панель суммы Расходы/Доходы)

---
+++ src/components/ReportHeader/ReportHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ReportHeader = () => {
+  return (
+    <div>
+      <div>
+        <p>
+          Расходы: <span></span>
+        </p>
+      </div>
+      <div>
+        <p>
+          Доходы: <span></span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ReportHeader;

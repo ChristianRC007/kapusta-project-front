0722d8977e74bc80ecb60f2a65badd559c12463a
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-10-10 20:53:27 +0200

    fix

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -11,14 +11,26 @@ class LoginForm extends Component {
     name: '',
     email: '',
     password: '',
+
+    showAttention:false,
   };
+
+
+
 
   handleSubmit = e => {
     e.preventDefault();
 
     this.props.onLogin(this.state);
+   
+     if (!this.name && !this.email && !this.password) {
+        this.setState({showAttention:true});
+    }
+
     this.setState({ name: '', email: '', password: '' });
   };
+
+
 
   handleChange = e => {
     this.setState({
@@ -29,6 +41,7 @@ class LoginForm extends Component {
   render() {
     const { name, email, password } = this.state;
     const { handleSubmit, handleChange } = this;
+
 
     return (
       <div className="container_form">
@@ -48,6 +61,7 @@ class LoginForm extends Component {
               зарегистрировавшись:
             </p>
             <label className="label">
+              {this.showAttention?<sup className='form__starAttantion'>*</sup> :''}
               Имя:
               <input
                 className="input"
@@ -57,14 +71,17 @@ class LoginForm extends Component {
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
                 placeholder="Ваше имя"
-                required
+                // required
+                autoComplete="off"
                 minLength="3"
+        
                 onChange={handleChange}
               />
             </label>
+           {this.showAttention?<p className='Form__textAttantion'>это обязательное поле</p>:''}
 
             <label className="label">
-              Электронная почта:
+            {this.showAttention?<sup className='form__starAttantion'>*</sup> :''} Электронная почта:
               <input
                 className="input"
                 type="email"
@@ -72,13 +89,15 @@ class LoginForm extends Component {
                 value={email}
                 placeholder="your@email.com"
                 title="Введите свою пошту."
-                required
+                // required
+                 autoComplete="off"
                 onChange={handleChange}
               />
             </label>
+            {this.showAttention?<p className='Form__textAttantion'>это обязательное поле</p>:''}
 
             <label className="label">
-              Пароль:
+           {this.showAttention?<sup className='form__starAttantion'>*</sup> :''}   Пароль:
               <input
                 className="input"
                 onChange={handleChange}
@@ -87,10 +106,12 @@ class LoginForm extends Component {
                 value={password}
                 placeholder="Пароль"
                 title="Пароль больше 6-ти символов"
-                required
+                // required
                 minLength="6"
+                autoComplete="off"
               />
             </label>
+            {this.showAttention?<p className='Form__textAttantion'>это обязательное поле</p>:''}
           </div>
 
           <div className="button_container">
@@ -114,6 +135,7 @@ class LoginForm extends Component {
 
 const mapDispatchToProps = {
   onLogin: authOperations.register,
+  isAuthenticated: authSelectors.getIsAuthenticated(state),
 };
 
 export default connect(null, mapDispatchToProps)(LoginForm);

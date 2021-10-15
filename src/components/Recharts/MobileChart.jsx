57a646202160f4a7e051239024fc91f0260aa77f
VolodymyrVorona <volodimir.vorona@gmail.com>
2021-10-16 01:46:16 +0300

    Moved components to the function

--- src/components/Recharts/MobileChart.jsx
+++ src/components/Recharts/MobileChart.jsx
@@ -8,38 +8,54 @@ import {
   LabelList,
 } from 'recharts';
 
-import { MobileBarLabel, MobileCategoryLabel } from './StylesForLabel';
+const MobileCharts = ({ data }) => {
+  const MobileBarLabel = ({ x, y, width, value }) => (
+    <text
+      x={x + width / 1.1}
+      y={y}
+      textAnchor="middle"
+      fontSize={10}
+      dy={-10}
+    >{`${value} грн`}</text>
+  );
 
-const MobileCharts = ({ data }) => (
-  <ResponsiveContainer width="100%" height={500}>
-    <BarChart
-      layout="vertical"
-      data={data}
-      margin={{ top: 20, right: 0, bottom: 30, left: 0 }}
-      className="chartText"
-    >
-      <Bar
-        dataKey="amount"
-        barSize={18}
-        radius={[0, 10, 10, 0]}
-        label={<MobileBarLabel />}
-        fill="#52555f"
-        minPointSize={80}
+  const MobileCategoryLabel = ({ x, y, value }) => (
+    <text x={x} y={y} dy={-10} fontSize={10}>
+      {value}
+    </text>
+  );
+
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart
+        layout="vertical"
+        data={data}
+        margin={{ top: 30, right: 0, bottom: 30, left: 0 }}
+        className="chartText"
       >
-        {data.map((el, idx) => (
-          <Cell key={`cell-${idx}`} fill={idx % 3 ? '#FFDAC0' : '#ff751d'} />
-        ))}
-        <LabelList
-          dataKey="category"
-          content={<MobileCategoryLabel />}
-          fill="#52555F"
-        />
-      </Bar>
+        <Bar
+          dataKey="amount"
+          barSize={18}
+          radius={[0, 10, 10, 0]}
+          label={<MobileBarLabel />}
+          fill="#52555f"
+          minPointSize={80}
+        >
+          {data.map((el, idx) => (
+            <Cell key={`cell-${idx}`} fill={idx % 3 ? '#FFDAC0' : '#ff751d'} />
+          ))}
+          <LabelList
+            dataKey="category"
+            content={<MobileCategoryLabel />}
+            fill="#52555F"
+          />
+        </Bar>
 
-      <XAxis type="number" hide={true} />
-      <YAxis dataKey="category" type="category" scale="band" hide={true} />
-    </BarChart>
-  </ResponsiveContainer>
-);
+        <XAxis type="number" hide={true} />
+        <YAxis dataKey="category" type="category" scale="band" hide={true} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default MobileCharts;

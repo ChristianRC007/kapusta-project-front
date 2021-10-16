aee97b64bf11f60d9a60f949f2a047f819e9a22f
VolodymyrVorona <volodimir.vorona@gmail.com>
2021-10-17 02:57:22 +0300

    Підключив список категорій

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect } from 'react';
 import ReportHeader from '../../components/ReportHeader/ReportHeader';
 import BalanceHeader from '../../components/BalanceHeader/BalanceHeader';
-import WestInCome from '../../components/WestInCome';
+// import WestInCome from '../../components/WestInCome';
 import Rechart from '../../components/Recharts';
 import { useDispatch, useSelector } from 'react-redux';
-import { reportsOperations, reportsSelectors } from '../../redux/reports';
+import {
+  reportsOperations,
+  reportsSelectors,
+  reportsActions,
+} from '../../redux/reports';
+import ReportsList from '../../components/Reports/ReportsList';
 
 import { format } from 'date-fns';
 
 export default function ReportPage() {
   const dispatch = useDispatch();
-  const costs = useSelector(reportsSelectors.getCosts);
+  // const costs = useSelector(reportsSelectors.getCosts);
   const getExpenseDetail = useSelector(reportsSelectors.getExpenseDetail);
-  const getIncomeDetail = useSelector(reportsSelectors.getIncomeDetail);
+  // const getIncomeDetail = useSelector(reportsSelectors.getIncomeDetail);
 
   useEffect(() => {
     const formatDate = format(new Date(), 'yyyy-MM');
@@ -20,11 +25,32 @@ export default function ReportPage() {
     dispatch(reportsOperations.getIncomeDetail(formatDate)); //!costs
   }, [dispatch]);
 
+  const setActiveCategory = (arr, category) => {
+    const array = arr?.map(el => {
+      if (el.isActive) {
+        return { ...el, isActive: !el.isActive };
+      }
+
+      if (el._id === category) {
+        return { ...el, isActive: !el.isActive };
+      }
+      return el;
+    });
+
+    return array;
+  };
+
+  const handleClick = category => {
+    const expanse = setActiveCategory(getExpenseDetail, category);
+    dispatch(reportsActions.setActiveExpanse(expanse));
+  };
+
   return (
     <>
       <BalanceHeader />
       <ReportHeader />
-      <WestInCome data={costs ? getExpenseDetail : !costs && getIncomeDetail} />
+      <ReportsList transactions={getExpenseDetail} onClick={handleClick} />
+      {/* <WestInCome data={costs ? getExpenseDetail : !costs && getIncomeDetail} /> */}
       <Rechart />
     </>
   );
